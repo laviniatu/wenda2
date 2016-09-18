1bf77affb3e54a47674545e2557e776e93644d4f
laviniatu <laviniatu@163.com>
2016-09-18 21:00:10 +0800

    Initial commit

---
+++ src/main/java/com/nowcoder/service/QuestionService.java
@@ -0,0 +1,43 @@
+package com.nowcoder.service;
+
+import com.nowcoder.dao.QuestionDAO;
+import com.nowcoder.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.List;
+
+/**
+ * Created by nowcoder on 2016/7/15.
+ */
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDAO questionDAO;
+
+    @Autowired
+    SensitiveService sensitiveService;
+
+    public Question getById(int id) {
+        return questionDAO.getById(id);
+    }
+
+    public int addQuestion(Question question) {
+        question.setTitle(HtmlUtils.htmlEscape(question.getTitle()));//过滤掉html标签，会进行一些转义，显示出来的还是有
+        question.setContent(HtmlUtils.htmlEscape(question.getContent()));
+        // 敏感词过滤
+        question.setTitle(sensitiveService.filter(question.getTitle()));
+        question.setContent(sensitiveService.filter(question.getContent()));
+        return questionDAO.addQuestion(question) > 0 ? question.getId() : 0;//问题添加成功，返回问题ID，否则返回0
+    }
+
+    public List<Question> getLatestQuestions(int userId, int offset, int limit) {
+        return questionDAO.selectLatestQuestions(userId, offset, limit);
+    }
+
+    public int updateCommentCount(int id, int count) {
+        return questionDAO.updateCommentCount(id, count);
+    }
+}
